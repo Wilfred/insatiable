9d1ef85e2bc11044ef7ba1ec398748964846d796
Wilfred Hughes <me@wilfred.me.uk>
2018-07-01 16:22:19 +0100

    Formatting

--- insatiable.ml
+++ insatiable.ml
@@ -1,24 +1,20 @@
-
 type variable = int
 
-type binding = (variable * bool)
+type binding = variable * bool
+
 type assignment = binding list
 
-type literal =
-  | Var of variable
-  | NegatedVar of variable;;
+type literal = Var of variable | NegatedVar of variable
 
 (* Literals joined by OR. *)
-type clause = literal list;;
+type clause = literal list
 
 (* Clauses joined by AND. *)
-type expr = clause list;;
+type expr = clause list
 
-type sat_result =
-  | Unsatisfiable
-  | Satisfiable of assignment;;
+type sat_result = Unsatisfiable | Satisfiable of assignment
 
-let possible_bindings (cl : clause) : binding list =
+let possible_bindings (cl: clause) : binding list =
   List.map
     (fun var -> match var with
          (Var v) -> (v, true)
@@ -32,29 +28,29 @@ let opt_map f v =
   | None -> None;;
 
 let push_head v vs =
-  v::vs;;
+  v :: vs;;
 
 let rec all_some items =
   match items with
     [] -> true
-  | (x::xs) ->
+  | x :: xs ->
     match x with
       (Some _) -> all_some xs
     | None -> false;;
 
-let rec flat_opt_list (items : 'a option list) : 'a list option =
+let rec flat_opt_list (items: 'a option list) : 'a list option =
   match items with
     [] -> Some []
-  | (x::xs) ->
+  | x :: xs ->
     (match (x, flat_opt_list xs) with
-      (Some x', Some xs') -> Some (x'::xs')
+      (Some x', Some xs') -> Some (x' :: xs')
     | _ -> None);;
     
-let rec clause_assign (cl : clause) (b : binding) : clause option =
+let rec clause_assign (cl: clause) (b: binding) : clause option =
   let (var, var_val) = b in
   match cl with
     [] -> Some cl
-  | ((Var v)::vs) ->
+  | (Var v) :: vs ->
     if v == var then
       (if var_val then
         clause_assign vs b
@@ -62,7 +58,7 @@ let rec clause_assign (cl : clause) (b : binding) : clause option =
         None)
     else
       opt_map (push_head (Var v)) (clause_assign vs b) 
-  | ((NegatedVar v)::vs) ->
+  | (NegatedVar v) :: vs ->
     if var == v then
       (if var_val then
         None
@@ -74,10 +70,10 @@ let rec clause_assign (cl : clause) (b : binding) : clause option =
 (* Apply assignment to this clause. Return the new clause if the
    assignment can be applied, or None if the clause cannot be
    satisfied with this assignment. *)
-let rec simplify (cl : clause) (a : assignment) : clause option =
+let rec simplify (cl: clause) (a: assignment) : clause option =
   match a with
     [] -> Some cl
-  | (b::bs) ->
+  | b :: bs ->
     match clause_assign cl b with
       Some cl' -> simplify cl' bs
     | None -> None;;
@@ -85,24 +81,24 @@ let rec simplify (cl : clause) (a : assignment) : clause option =
 (* Given a SAT expression and a list of permissible assignments,
    attempt to find an assignment that satisfies the entire
    expression. *)
-let rec satisfy (e : expr) (a : assignment) : assignment option =
+let rec satisfy (e: expr) (a: assignment) : assignment option =
   match e with
     [] -> Some a
-  | (c::cs) ->
+  | c :: cs ->
     (* Get all the possible satisfying assignments for this first clause. *)
     let rec find_assignment binds =
       match binds with
         [] ->
         (* Ran out of possible assignments that we could try. *)
         None
-      | (b::bs) ->
+      | b :: bs ->
         (* Try this assignment on the remaining clauses. *)
         let remaining = List.map (fun cl -> clause_assign cl b) cs in
         (match (flat_opt_list remaining) with
            (Some remaining') ->
            (* If this assignment worked for all the other clauses, keep
               going. *)
-           (match (satisfy remaining' (b::a)) with
+           (match (satisfy remaining' (b :: a)) with
               (Some final_a) ->
               (* We managed to find an assignmentent continuing from the
                  current assignments. *)
--- test.ml
+++ test.ml
@@ -3,18 +3,14 @@
 
 let flatten_some_list () =
   let result = Insatiable.flat_opt_list [Some 1; Some 2] in
-  Alcotest.(check (option (list int))) "same lists" (Some [1; 2]) result;;
+  Alcotest.(check (option (list int))) "same lists" (Some [1; 2]) result
 
 let flatten_mixed_list () =
   let result = Insatiable.flat_opt_list [Some 1; None; Some 2] in
-  Alcotest.(check (option (list int))) "both none" None result;;
+  Alcotest.(check (option (list int))) "both none" None result
 
-let test_set = [
-  "Flatten list", `Quick , flatten_some_list;
-  "Flatten mixed list", `Quick , flatten_mixed_list;
-]
+let test_set =
+  [ ("Flatten list", `Quick, flatten_some_list)
+  ; ("Flatten mixed list", `Quick, flatten_mixed_list) ]
 
-let () =
-  Alcotest.run "Unit tests" [
-    "test_set", test_set;
-  ]
+let () = Alcotest.run "Unit tests" [("test_set", test_set)]
